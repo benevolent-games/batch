c769ae6fba8fcd202d9c5656a753b63ef35ead7a
Chase Moskal <chasemoskal@gmail.com>
2024-05-28 19:06:14 -0700

    fix: assert directories to allow empty path array

--- s/tools/assert-directories.ts
+++ s/tools/assert-directories.ts
@@ -4,6 +4,7 @@ import {dirname} from "path"
 
 /** given an array of file paths, use `mkdir -p` to ensure their directories exist */
 export async function assertDirectories(paths: string[]) {
-	await $`mkdir -p ${paths.map(p => dirname(p))}`
+	if (paths.length > 0)
+		await $`mkdir -p ${paths.map(p => dirname(p))}`
 }
 
